104159e770bd7e24d0cdab291228f38c978c1a60
Davidson Ratis <davidsonmakarios@gmail.com>
2024-05-31 15:54:28 -0300

    created - FeedbackHeader component

---
+++ src/components/FeedbackHeader/index.tsx
@@ -0,0 +1,8 @@
+export const FeedbackHeader = () => {
+  return (
+    <header className="relative">
+      <p>Deixe seu Feedback</p>
+      <button className="absolute right-0 top-0 text-lg text-slate-400 hover:text-white">x</button>
+    </header>
+  )
+}
