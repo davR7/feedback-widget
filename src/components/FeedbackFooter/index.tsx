bfd0dd1b470dab6356804c27171338003bfa22da
Davidson Ratis <davidsonmakarios@gmail.com>
2024-05-31 15:58:04 -0300

    created - FeedbackFooter component

---
+++ src/components/FeedbackFooter/index.tsx
@@ -0,0 +1,10 @@
+export const FeedbackFooter = () => {
+  return (
+    <footer className="">
+      Feito por{" "}
+      <a className="underline underline-offset-4" href="https://github.com/davR7" target="_blank">
+        davR7
+      </a>
+    </footer>
+  )
+}
