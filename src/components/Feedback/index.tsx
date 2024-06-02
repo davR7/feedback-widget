3b07a727dc77afae9821968bce479d291123465f
Davidson Ratis <davidsonmakarios@gmail.com>
2024-06-02 13:32:46 -0300

    updated - improved Feedback component, version final

--- src/components/Feedback/index.tsx
+++ src/components/Feedback/index.tsx
@@ -1,17 +1,28 @@
-import { FeedbackHeader } from "../FeedbackHeader"
-import { FeedbackMain } from "../FeedbackMain"
-import { FeedbackFooter } from "../FeedbackFooter"
-import { FeedbackButton } from "../FeedbackButton"
+"use client";
+
+import { FeedbackHeader } from "../FeedbackHeader";
+import { FeedbackMain } from "../FeedbackMain";
+import { FeedbackFooter } from "../FeedbackFooter";
+import { FeedbackButton } from "../FeedbackButton";
+import { FeedbackProvider } from "@/context/feedback/FeedbackProvider";
+import { useState } from "react";
+import { FeedbackSuccess } from "../FeedbackMain/FeedbackSuccess";
 
 export const Feedback = () => {
+  const [feedbackOn, setFeedbackOn] = useState(false);
+
   return (
     <div className="absolute bottom-4 right-4 flex flex-col items-end">
-      <div className="mb-4 rounded-lg bg-zinc-900 p-4">
-        <FeedbackHeader />
-        <FeedbackMain />
-        <FeedbackFooter />
-      </div>
-      <FeedbackButton />
+      {feedbackOn && (
+        <div className="mb-4 rounded-lg bg-zinc-900 p-4">
+          <FeedbackProvider>
+            <FeedbackHeader onClick={() => setFeedbackOn(prev => !prev)} />
+            <FeedbackMain />
+            <FeedbackFooter />
+          </FeedbackProvider>
+        </div>
+      )}
+      <FeedbackButton onClick={() => setFeedbackOn(prev => !prev)} />
     </div>
-  )
-}
+  );
+};
