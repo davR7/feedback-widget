040d358857e3a4ae5923b85827c2e488c2fa2a65
Davidson Ratis <davidsonmakarios@gmail.com>
2024-06-02 13:03:26 -0300

    created - data for feedback options

---
+++ src/components/FeedbackMain/options.ts
@@ -0,0 +1,26 @@
+export const options = [
+  {
+    id: 1,
+    title: "Problema",
+    text: "Conte com detalhes o problema.",
+    type: "BUG",
+    image: { source: "/bug.svg", alt: "Icone de um Inseto" },
+    callback: "SUCCESS",
+  },
+  {
+    id: 2,
+    title: "Ideia",
+    text: "Queremos ouvir suas sugestões :)",
+    type: "IDEA",
+    image: { source: "/idea.svg", alt: "Icone de uma Lâmpada" },
+    callback: "SUCCESS",
+  },
+  {
+    id: 3,
+    title: "Outro",
+    text: "Estamos ansiosos para ouvi-lo(a).",
+    type: "OTHER",
+    image: { source: "/thought.svg", alt: "Icone de uma Nuvem" },
+    callback: "SUCCESS",
+  },
+];
