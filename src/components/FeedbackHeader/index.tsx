94b1886b26fcb9acc45c72fb3d478c4b81f0d7e7
Davidson Ratis <davidsonmakarios@gmail.com>
2024-06-02 13:29:07 -0300

    updated - improved FeedbackHeader component, version final

--- src/components/FeedbackHeader/index.tsx
+++ src/components/FeedbackHeader/index.tsx
@@ -1,8 +1,28 @@
-export const FeedbackHeader = () => {
+import { useFeedback } from "@/hook/useFeedback";
+import { FaArrowLeft } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
+
+import Image from "next/image";
+
+export const FeedbackHeader = ({ onClick }: { onClick: () => void }) => {
+  const ctx = useFeedback();
+
   return (
     <header className="relative">
-      <p>Deixe seu Feedback</p>
-      <button className="absolute right-0 top-0 text-lg text-slate-400 hover:text-white">x</button>
+      {!ctx.callback && ctx.feedback.title && (
+        <button className="absolute left-0 top-2 text-slate-400 hover:text-white" onClick={ctx.resetFeedback}>
+          <FaArrowLeft size={15} />
+        </button>
+      )}
+      <div className="flex items-center justify-center">
+        {ctx.feedback.title && (
+          <Image src={ctx.feedback.image.source} width={25} height={25} alt={ctx.feedback.image.alt} />
+        )}
+        <h3 className="ml-2 text-lg">{ctx.feedback.title ? ctx.feedback.title : "Deixe seu Feedback"}</h3>
+      </div>
+      <button className="absolute right-0 top-1 text-lg text-slate-400 hover:text-white" onClick={onClick}>
+        <IoMdClose size={20} />
+      </button>
     </header>
-  )
-}
+  );
+};
