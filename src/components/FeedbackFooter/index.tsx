3093d4f94b4bd38dd47037c2342b871ccbff3454
Davidson Ratis <davidsonmakarios@gmail.com>
2024-06-02 13:30:06 -0300

    updated - improved FeedbackFooter component, version final

--- src/components/FeedbackFooter/index.tsx
+++ src/components/FeedbackFooter/index.tsx
@@ -1,10 +1,14 @@
 export const FeedbackFooter = () => {
   return (
-    <footer className="">
+    <footer className="text-center">
       Feito por{" "}
-      <a className="underline underline-offset-4" href="https://github.com/davR7" target="_blank">
+      <a
+        className="underline underline-offset-4 hover:text-blue-400"
+        href="https://github.com/davR7"
+        target="_blank"
+      >
         davR7
       </a>
     </footer>
-  )
-}
+  );
+};
