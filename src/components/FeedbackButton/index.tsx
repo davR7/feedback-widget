7eb3d6eb23ea50fb6ae198d2e7aff630372b1052
Davidson Ratis <davidsonmakarios@gmail.com>
2024-06-02 13:31:02 -0300

    updated - improved FeedbackButton component, version final

--- src/components/FeedbackButton/index.tsx
+++ src/components/FeedbackButton/index.tsx
@@ -1,12 +1,12 @@
-import { AiOutlineMessage } from "react-icons/ai"
+import { AiOutlineMessage } from "react-icons/ai";
 
-export const FeedbackButton = () => {
+export const FeedbackButton = ({ onClick }: { onClick: () => void }) => {
   return (
-    <button className="group mt-2 flex items-center rounded-full bg-blue-500 p-3">
+    <button className="group mt-2 flex items-center rounded-full bg-blue-500 p-3" onClick={onClick}>
       <AiOutlineMessage size={30} />
       <p className="max-w-0 overflow-hidden transition-all duration-500 ease-in-out group-hover:block group-hover:max-w-xs group-hover:pl-1">
         Feedback
       </p>
     </button>
-  )
-}
+  );
+};
